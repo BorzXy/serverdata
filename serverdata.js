9c94e02091d3583bf90c99c881e89a1ddf38836d
ImDanilo <66525747+daniloanantaa@users.noreply.github.com>
2021-01-15 23:34:44 +0800

    Add files via upload

---
+++ serverdata.js
@@ -0,0 +1,23 @@
+let request = require('request');
+const fs = require('fs');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+}); 
+
+console.log('server_data.php Maker by BorzXy')
+readline.question('IP : ', ip => {
+readline.question('PORT: ', port => {
+readline.question('MAINT: ', maint => {
+
+let data = (`server|${ip}\nport|${port}\ntype|1\n#maint|${maint}\n\nbeta_server|127.0.0.1\nbeta_port|17091\n\nbeta_type|1\nmeta|localhost\nRTENDMARKERBS1001`)
+
+fs.writeFile('server_data.php', data, (err) => { 
+     
+if (err) throw err; 
+console.log('Success...')
+})
+
+})
+})
+})
---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+cls
+title Server_data.php Maker By ImDanilo aka BorzXy
+node serverdata.js
+pause
